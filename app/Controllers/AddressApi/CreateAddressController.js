9a9033ddfccf074437f09c6aa72fa959e86f5779
crocodiles128 <luquinhasjoserocha@gmail.com>
2026-05-01 16:44:29 -0300

    NEW|rotas e controllers de endereco

---
+++ app/Controllers/AddressApi/CreateAddressController.js
@@ -0,0 +1,34 @@
+import postgres from '../../../database/connections/postgres.js';
+
+export default async function CreateAddressController(request, response) {
+    try {
+        const { name, district, city } = request.body;
+
+        const error = [];
+
+        if (!name) {
+            error.push("name obrigatório!");
+        }
+
+        if (!district) {
+            error.push("district obrigatório!");
+        }
+
+        if (!city) {
+            error.push("city obrigatório!");
+        }
+
+        if (error.length > 0) {
+            return response.status(400).json({ error: error });
+        }
+
+        const result = await postgres.query(
+            'INSERT INTO adresses (name, district, city) VALUES ($1, $2, $3) RETURNING *',
+            [name, district, city]
+        );
+        return response.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        return response.status(500).json({ error: 'Internal server error' });
+    }
+}
--- routes/apis/addressRouter.js
+++ routes/apis/addressRouter.js
@@ -1,15 +1,22 @@
-
-
-import { Router } from "express";
+import { Router } from 'express';
+import ListAddressController from '../../app/Controllers/AddressApi/ListAddressController.js';
+import GetAddressController from '../../app/Controllers/AddressApi/GetAddressController.js';
+import CreateAddressController from '../../app/Controllers/AddressApi/CreateAddressController.js';
+import UpdateAddressController from '../../app/Controllers/AddressApi/UpdateAddressController.js';
+import DeleteAddressController from '../../app/Controllers/AddressApi/DeleteAddressController.js';
 
 export default (() => {
     const router = Router();
 
-    /** 
-     * TF 09
-     * 
-     * Insira as 5 rotas aqui
-     */
+    router.get('/', ListAddressController);
+
+    router.get('/:id', GetAddressController);
+
+    router.post('/', CreateAddressController);
+
+    router.put('/:id', UpdateAddressController);
+
+    router.delete('/:id', DeleteAddressController);
 
     return router;
 })();
